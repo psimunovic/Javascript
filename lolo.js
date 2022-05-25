6bcac779a590abd3a1bdaf0d9e7b5278a2f50c4f
Paulo Simunovic <91575657+psimunovic@users.noreply.github.com>
2022-05-24 22:38:34 -0300

    update 2

---
+++ lolo.js
@@ -0,0 +1,11 @@
+var nombre;
+nombre = window.prompt('ingresa tu nombre');
+window.alert(nombre);
+
+var numero1, numero2, resultado;
+
+
+numero1 = Number(window.prompt('ingrese el 1er numero: '));
+numero2 = Number(window.prompt('ingrese el 2do numero: '));
+resultado = numero1 + numero2;
+window.alert('EL RESULTADO ES: ' + String(resultado));
