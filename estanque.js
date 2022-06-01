ec376b482ba900069b1ce0a28872130552685e4c
Paulo Simunovic <91575657+psimunovic@users.noreply.github.com>
2022-05-31 22:29:43 -0300

    Version 2 del Js del programa del estanque
    Se le agrega el control de excesos

--- estanque.js
+++ estanque.js
@@ -1,9 +1,14 @@
-var litros_actuales;
+var litros_actuales, exceso;
 
 
 litros_actuales = Number(window.prompt('ingrese la cantidad de litros actuales del estanque '));
 while (litros_actuales < 10) {
   litros_actuales = litros_actuales + 1;
   window.alert('litros aumentando en ... ' + String(litros_actuales));
+}
+if (litros_actuales > 10) {
+  exceso = litros_actuales - 10;
+  litros_actuales = litros_actuales - exceso;
+  window.alert('tuvo un exceso de : ' + String(exceso));
 }
 window.alert('estanque lleno');
